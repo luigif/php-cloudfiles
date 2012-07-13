1f32f7ee053c71443da6e8d4562006ed0fc3cf74
omissis <claudi0.beatric3@gmail.com>
2012-07-13 11:27:15 +0200

    tests updated

--- tests/Rackspace/Tests/CloudFiles/AuthenticationTest.php
+++ tests/Rackspace/Tests/CloudFiles/AuthenticationTest.php
@@ -4,26 +4,42 @@ namespace Rackspace\Tests\Cloudfiles;
 
 use Rackspace\Cloudfiles\Authentication;
 use Rackspace\Cloudfiles\Connection;
+use Rackspace\Cloudfiles\CoreConfig;
+use Rackspace\Cloudfiles\Authentication\Config as AuthenticationConfig;
 
 require_once 'common.php';
 
 class AuthenticationTest extends \PHPUnit_Framework_TestCase
 {
-    function __construct() {
-        $this->auth = null;
+    private $authenticationConfig;
+    private $fixturesDir;
+    private $coreConfig;
+
+    public function __construct() {
+        $this->authenticationConfig = null;
+        $this->fixturesDir          = realpath(__DIR__ . '/../Fixtures');
+        $this->coreConfig           = null;
     }
 
     protected function setUp() {
-        $this->auth = new Authentication(USER, API_KEY);
-        $this->auth->authenticate();
-        $conn = new Connection($this->auth);
+        $this->coreConfig = new CoreConfig(array(
+            'cloudFilesCABundlePath' => $this->fixturesDir . '/cacert.pem',
+            'cloudFilesMagicPath'    => $this->fixturesDir . '/magic',
+        ));
+
+        $this->authenticationConfig = new AuthenticationConfig($this->coreConfig, array(
+            'username' => USER,
+            'apiKey'   => API_KEY,
+            'account'  => ACCOUNT,
+        ));
     }
 
     public function testTokenCache()
     {
-        $this->auth =  new Authentication(USER, API_KEY);
-        $this->auth->authenticate();
-        $arr = $this->auth->export_credentials();
+        $auth =  new Authentication($this->authenticationConfig);
+        $auth->authenticate();
+
+        $arr = $auth->export_credentials();
         $this->assertNotNull($arr['storage_url']);
         $this->assertNotNull($arr['cdnm_url']);
         $this->assertNotNull($arr['auth_token']);
@@ -31,26 +47,31 @@ class AuthenticationTest extends \PHPUnit_Framework_TestCase
 
     public function testTokenAuth()
     {
-        $this->auth =  new Authentication(USER, API_KEY);
-        $this->auth->authenticate();
-        $arr = $this->auth->export_credentials();
-        $this->assertNotNull($arr['storage_url']);
-        $this->assertNotNull($arr['cdnm_url']);
-        $this->assertNotNull($arr['auth_token']);
-        $this->auth = new Authentication();
-        $this->auth->load_cached_credentials($arr['auth_token'], $arr['storage_url'], $arr['cdnm_url']);
-        $conn = new Connection($this->auth);
-    }
-
-    public function testTokenErrors()
-    {
-        $auth =  new Authentication(USER, API_KEY);
+        $auth =  new Authentication($this->authenticationConfig);
         $auth->authenticate();
+
         $arr = $auth->export_credentials();
         $this->assertNotNull($arr['storage_url']);
         $this->assertNotNull($arr['cdnm_url']);
         $this->assertNotNull($arr['auth_token']);
-        $this->auth = new Authentication();
+
+        $auth = new Authentication($this->authenticationConfig);
+        $auth->load_cached_credentials($arr['auth_token'], $arr['storage_url'], $arr['cdnm_url']);
+        $conn = new Connection($this->coreConfig, $auth);
+    }
+
+    public function testTokenErrors()
+    {
+        $auth =  new Authentication($this->authenticationConfig);
+        $auth->authenticate();
+
+        $arr = $auth->export_credentials();
+        $this->assertNotNull($arr['storage_url']);
+        $this->assertNotNull($arr['cdnm_url']);
+        $this->assertNotNull($arr['auth_token']);
+
+        $auth = new Authentication($this->authenticationConfig);
+
         $this->setExpectedException('Rackspace\\Exception\\SyntaxException');
         $auth->load_cached_credentials(NULL, $arr['storage_url'], $arr['cdnm_url']);
         $this->setExpectedException('Rackspace\\Exception\\SyntaxException');
@@ -62,23 +83,30 @@ class AuthenticationTest extends \PHPUnit_Framework_TestCase
     public function testBadAuthentication()
     {
         $this->setExpectedException('Rackspace\\Exception\\AuthenticationException');
-        $auth = new Authentication('e046e8db7d813050b14ce335f2511e83', 'bleurrhrhahra');
+
+        $authenticationConfig = new AuthenticationConfig($this->coreConfig, array(
+            'username' => 'foo',
+            'apiKey'   => 'bar',
+        ));
+
+        $auth =  new Authentication($authenticationConfig);
         $auth->authenticate();
     }
 
     public function testAuthenticationAttributes()
     {
-        $this->assertNotNull($this->auth->storage_url);
-        $this->assertNotNull($this->auth->auth_token);
+        $auth =  new Authentication($this->authenticationConfig);
+        $auth->authenticate();
+
+        $this->assertNotNull($auth->getConfiguration()->getStorageUrl());
+        $this->assertNotNull($auth->getConfiguration()->getAuthToken());
 
         if (ACCOUNT) {
-            $this->assertNotNull($this->auth->cdnm_url);
+            $this->assertNotNull($auth->cdnm_url);
         }
     }
     public function testUkAuth()
     {
-        $this->assertTrue(UK_AUTHURL === 'https://lon.auth.api.rackspacecloud.com');
+        $this->assertTrue($this->coreConfig->getAuthUrl('uk') === 'https://lon.auth.api.rackspacecloud.com');
     }
 }
-
-?>
